65db2ef310f9f5c465120456e91dc2f6326e3b05
Mark Drew <mark.drew@gmail.com>
2012-10-26 09:49:34 +0100

    Adding checks for various startup things

--- railo.sh
+++ railo.sh
@@ -3,9 +3,6 @@ CURRPATH=$(pwd)
 #java -jar ${railoCLIPATH} -webroot=${webroot} -uri=/runner.cfm
 PARAMS=""
 #Check the first param... 
-echo $0
-echo $1
-
 ##If param $1 has no length, list commands and list cli-extensions
 
 ##If param $1 is run
@@ -17,4 +14,20 @@ do
     PARAMS=${PARAMS}~"$var"
 done
 cd $(dirname $0)
+if [ -n "$1" ]
+then
+
+	if [ "$1" == "run" ]/Applications/Railo-4-CLI/
+	then
+		echo "WE are going to run a file, so we just do the file"
+		## java -jar ./lib/ext/railo-cli.jar -webroot=./webroot/ -uri='/runner.cfm?currentpath='${CURRPATH}'&params='$PARAMS
+		
+	fi
+	
+	if [ "$1" == "start" ]
+	then
+		echo "WE are going to run a file, so we just do the file"
+		java -DSTOP.PORT=8887 -DSTOP.KEY=railo -jar -Xms256M  -Xmx512M lib/start.jar -Drespath=$CURRPATH		
+	fi
+fi
 #java -jar ./lib/ext/railo-cli.jar -webroot=./webroot/ -uri='/runner.cfm?currentpath='${CURRPATH}'&params='$PARAMS
